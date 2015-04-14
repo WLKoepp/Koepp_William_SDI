59d0058d42a71be25c88a5ceda1e730af455362d
WLKoepp <wlkoepp@fullsail.edu>
2015-04-13 21:53:22 -0500

    Final Program of JS file

---
+++ AssociatingJS/JS/Script.js
@@ -0,0 +1,41 @@
+/**
+ * Created by williamkoepp on 4/6/15.
+ */
+// alert("Testing");
+
+//--------------------------//
+//var myFirstVariable;
+//myFirstVariable = "world";
+//alert(myFirstVariable);
+
+//-------------------------//
+//var string1 = "Hello";
+//var string2 = "Goodbye";
+
+//alert (string1);
+//alert (string2);
+
+//string2 = string1;
+
+//alert (string1);
+//alert (string2);
+
+//string1 = "Now for something different";
+
+//alert (string1);
+//alert (string2);
+
+var voltage;
+var current;
+var resistance;
+var wattage;
+var reply;
+
+reply = prompt ("what value are you looking for? voltage, current, resistance, or wattage. please type the full word in lowercase.");
+    if (reply = wattage){
+        voltage = prompt ("what is the voltage? (this is a numerical value");
+        current = prompt ("what is the amperage? (this is a numerical value");
+        wattage = voltage * current;
+        alert ("the wattage is")(wattage);
+
+    }
---
+++ AssociatingJS/Koepp_William_Expressions_Assignment/ThreePhaseCalculator.js
@@ -0,0 +1,65 @@
+/**
+ * Created by williamkoepp on 4/13/15.
+ */
+/*------------------------------------------------------------------------------------------*/
+//Initializing stage
+var watts = 0;              /*this is three phase watts , which is Voltage Average * Amperage Average * Power Factor * 1.732
+                            this is if a power factor is available. For rough estimates Voltage multiplied by Amperage will work.*/
+var volts = 0;
+var current = 0;
+var Pf = 0;
+var rms = 1.732;            //this is the root mean square value, half of Pi to represent half of a sinusoidal wave.
+var Hp = 0;                 /* Hp is short for Horsepower. A standard measurement for foot pounds per second,
+                            however this is in reference to electrical means, so 746 watts is 1 horsepower. */
+var efficiency = 0;
+/*------------------------------------------------------------------------------------------*/
+//Input stage
+alert ("Please enter all three values for Wattage, Voltage, and Amperage.");
+watts = prompt ("Please enter the measured motor wattage:");
+volts = prompt ("please enter the measured circuit voltage");
+current = prompt ("Please enter the measured circuit current");
+/*------------------------------------------------------------------------------------------*/
+//Formula & calculation stage
+Pf = (watts / (volts * current * rms)) * 100;  /*formula for three phase power factor of AC motors.
+                                                answered in a percentage */
+Hp = watts / 746;
+efficiency = ((Hp * 746) / (volts * current * Pf* rms)) * 100; //formula for 3 phase power efficiency.
+/*------------------------------------------------------------------------------------------*/
+//Output stage
+alert ("Your efficiency is :" + efficiency +  "\n Your motor Horsepower is:" + Hp + " \n Your motor power factor is :" + Pf + "%");
+
+/*-------------------------------------------------------------------------------------------
+testing stage
+Test 1
+input               outputs(actual)     manufacture specified
+208 Volts               x                   208V
+15 Amps(current)        x                   15Amp
+3730 Watts              x                      x
+x                       69.024Pf               x
+x                       5hp                    5hp
+x                       1 eff.                  .895
+
+Test 2
+ input               outputs(actual)     manufacture specified
+ 230 Volts               x                   230V
+ 13.6 Amps(current)      x                   13.6Amp
+ 3730 Watts              x                      x
+ x                       68.848Pf               x
+ x                       5hp                    5hp
+ x                       1 eff.                  .895
+
+ Test 3
+ input               outputs(actual)     manufacture specified
+ 460 Volts               x                   460V
+ 6.8 Amps(current)       x                   6.8Amp
+ 3730 Watts              x                      x
+ x                       68.848Pf               x
+ x                       5hp                    5hp
+ x                       1 eff.                  .895
+
+ these values are fictional and if to say the motor was at 460 Volt, the amperage could not exceed 5.25 Amp to have the
+ efficiency specified of .895
+ */
+
+
+
